0382ac7b1e5b89edc430b4111108ba9098099b47
Gayan <gayanhewa@gmail.com>
2015-03-18 23:23:23 +0800

    Adding the user migrations.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,26 @@ class CreateUsersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name');
+			$table->string('first')->nullable();
+			$table->string('last')->nullable();
+			$table->text('bio')->nullable();
+			$table->string('zip')->nullable();
+			$table->string('city')->nullable();
+			$table->string('state');
+			$table->string('country_code')->nullable();
+			$table->string('country')->nullable();
+			$table->string('phone')->nullable();
+			$table->string('facebook_token')->nullable();
+			$table->string('gplus_token')->nullable();
+			$table->string('twitter_token')->nullable();
+			$table->boolean('is_pro')->default(false);
+			$table->boolean('seller_location_verified')->default(false);
+			$table->boolean('verified')->default(false);
+			$table->decimal('response_time_hours')->default(0.00);
+			$table->string('timezone')->nullable();
 			$table->string('email')->unique();
 			$table->string('password', 60);
+			$table->boolean('is_admin')->default(false);
 			$table->rememberToken();
 			$table->timestamps();
 		});
