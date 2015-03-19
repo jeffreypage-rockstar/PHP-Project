25f552bed285e51b44c345d4d83a98896f7f4c5a
Gayan <gayanhewa@gmail.com>
2015-03-20 01:04:24 +0800

    Listing endpoint

---
+++ app/Dubb/Contracts/ListingInterface.php
@@ -0,0 +1,5 @@
+<?php namespace App\Dubb\Contracts;
+
+interface ListingInterface {
+
+}
---
+++ app/Dubb/Repos/EloquentListingRepository.php
@@ -0,0 +1,8 @@
+<?php namespace App\Dubb\Repos;
+
+use App\Dubb\Contracts\ListingInterface;
+
+class EloquentListingRepository implements ListingInterface
+{
+
+}
