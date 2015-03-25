79bd05cc262ea3bb898e9396b80447d2f4f2d6c3
Gayan <gayanhewa@gmail.com>
2015-03-25 22:26:38 +0800

    return the user id

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -34,7 +34,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $hidden = ['password', 'remember_token', 'id'];
+	protected $hidden = ['password', 'remember_token'];
 
 	public function listing()
 	{
